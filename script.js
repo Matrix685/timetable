1c438114ca5d4c6e45967aa4146592c4fe1f4019
Matrix685 <oflynnc48len@gmail.com>
2025-01-29 12:48:37 +0000

    temporary, hey by the way you should watch arcane

--- script.js
+++ script.js
@@ -11,4 +11,18 @@ for (const day of days) {
 
 console.log(timetable);
 
-timetable[1][2];
+addText("science", timetable[0][0]);
+addText("table tennis", timetable[1][1]);
+addText("twelve", timetable[2][2]);
+addText("WATCH ARCANE", timetable[3][3]);
+addText("science", timetable[4][4]);
+
+addText("science", timetable[0][5]);
+addText("science", timetable[1][4]);
+addText("science", timetable[2][3]);
+addText("science", timetable[3][2]);
+addText("science", timetable[4][1]);
+
+function addText(text, subject) {
+    subject.children[0].innerHTML = text;
+}
