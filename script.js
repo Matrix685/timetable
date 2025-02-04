0104f1e74e44a0ba9f3b31b1329138172151aff6
Matrix685 <oflynnc48len@gmail.com>
2025-02-04 09:24:59 +0000

    i PROMISE this isnt a scam

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const days = document.getElementsByClassName("classes");
+
 var timetable = [];
 var i = 0;
 
@@ -17,18 +18,49 @@ function buttonClicked() {
     document.getElementById("text").value = "";
 }
 
-addText("science", timetable[0][0]);
-addText("table tennis", timetable[1][1]);
-addText("twelve", timetable[2][2]);
-addText("WATCH ARCANE", timetable[3][3]);
-addText("science", timetable[4][4]);
+// addText("science", timetable[0][0]);
+// addText("table tennis", timetable[1][1]);
+// addText("twelve", timetable[2][2]);
+// addText("WATCH ARCANE", timetable[3][3]);
+// addText("science", timetable[4][4]);
 
-addText("science", timetable[0][5]);
-addText("science", timetable[1][4]);
-addText("science", timetable[2][3]);
-addText("science", timetable[3][2]);
-addText("science", timetable[4][1]);
+// addText("science", timetable[0][5]);
+// addText("science", timetable[1][4]);
+// addText("science", timetable[2][3]);
+// addText("science", timetable[3][2]);
+// addText("science", timetable[4][1]);
 
 function addText(text, subject) {
     subject.children[0].innerHTML = text;
 }
+
+function rand(array) {
+    var rand = Math.round(Math.random() * (array.length - 1));
+    console.log(rand);
+
+    return rand;
+}
+
+function chooseRand(subject) {
+    var randDays = rand(days);
+    var randPeriod = rand(Array(6));
+
+    if (timetable[randDays][randPeriod].children[0].innerHTML != "") chooseRand(subject);
+
+    addText(subject, timetable[randDays][randPeriod]);
+}
+
+var subjects = ["Béarla", "Gaeilge", "Mata"];
+
+for (const subject of subjects) {
+    // var randDays = rand(days);
+    // var randPeriod = rand(Array(6));
+
+    // if (timetable[randDays][randPeriod].children[0].innerHTML != "") continue;
+
+    // addText(subject, timetable[randDays][randPeriod]);
+
+    chooseRand(subject);
+}
+
+// alert("if you are reading this, i messed up somehow");
