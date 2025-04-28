0b18a17d3b83db8ae197eeeda567a37f6e185fe5
Matrix685 <oflynnc48len@gmail.com>
2025-04-28 23:59:23 +0100

    i wonder

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import { timetable, chooseRandRoom, chooseRandTile, fixArrays } from "../modules/timetable.js";
+import { timetable, chooseRandRoom, chooseRandTile, fixArrays } from "file:///C:/Users/Matrix/Documents/Coding/timetable/modules/timetable.mjs";
 
 // let tilesAnchor = [...tiles];
 // console.log(tilesAnchor);
