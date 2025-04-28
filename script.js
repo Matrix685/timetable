d6260fd8930d50cecf26d3781ad15f6390788a8f
altaltolympus <luke.ofoghlu@colaistechilliain.com>
2025-04-28 12:46:07 +0100

    righting great wrongs

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ let subjects = {
     ],
 };
 
-let rooms = ["Seomra Mór", "S5", "S9", "Bialann", "S1", "S2", "S3", "S4", "S6", "S7", "S8", "S10", "S11", "S12", "S13", "S14", "S15"];
+let rooms = ["Seomra Mór", "Bialann", "S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "S11", "S12", "S13", "S14", "S15"];
 let roomsAnchor = [...rooms];
 
 subjects.priomha.forEach((subject) => {
